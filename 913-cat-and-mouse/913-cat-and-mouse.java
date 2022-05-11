7174574938ae6225bdf7a106fd81b32a80547d17
Shreyansh Singh <65785547+ShreyanshSingh20@users.noreply.github.com>
2022-05-11 20:27:41 +0530

    Time: 464 ms (5.21%), Space: 68.2 MB (62.08%) - LeetHub

--- 913-cat-and-mouse/913-cat-and-mouse.java
+++ 913-cat-and-mouse/913-cat-and-mouse.java
@@ -1,39 +1,47 @@
 class Solution {
-    int[][][] dp;
     public int catMouseGame(int[][] graph) {
-        int n = graph.length;
-        dp = new int[4*n][n][n];
-        for(int i=0; i<dp.length; i++) for(int j=0;  j<dp[0].length; j++) Arrays.fill(dp[i][j],-1);
-        return seach(graph, 0, 1,2);
-    }
-    // t is which step, x is mouse location, y is cat location
-    int seach(int[][] graph, int t, int x, int y){
-        if (t == graph.length * 4) return 0;
-        if (x == y) return dp[t][x][y] = 2;
-        if (x == 0) return dp[t][x][y] = 1;
-        if (dp[t][x][y] != -1) return dp[t][x][y];
-         int who = t % 2;
-         boolean flag;
-        if (who == 0) { // mouse's turn
-            flag = true; // by default, is cat win
-            for (int i = 0; i < graph[x].length; i++) {
-                int nxt = seach(graph, t + 1, graph[x][i], y);
-                if (nxt == 1) return dp[t][x][y] = 1;
-                else if (nxt != 2) flag = false;
+        int n=graph.length;
+        int dp[][][]=new int[n+1][n+1][5*n+1];
+        for(int x[][]:dp){
+            for(int y[]:x){
+                Arrays.fill(y,-1);
             }
-            if (flag) return dp[t][x][y] = 2;
-            else return dp[t][x][y] = 0;
         }
-        else { // cat's turn
-            flag = true; // by default is mouse win
-            for (int i = 0; i < graph[y].length; i++)
-                if (graph[y][i] != 0) {
-                    int nxt = seach(graph, t + 1, x, graph[y][i]);
-                    if (nxt == 2) return dp[t][x][y] = 2;
-                    else if (nxt != 1) flag = false;
-                }
-            if (flag) return dp[t][x][y] = 1;
-            else return dp[t][x][y] = 0;
-        }
+        
+        return solver(graph,1,2,0,dp);
     }
+    
+    public int solver(int mat[][],int mousePos,int catPos,int time,int dp[][][]) {
+		
+		if(time==5*mat.length) return 0;
+		if(mousePos==0) return dp[mousePos][catPos][time]=1;
+		if(catPos==mousePos) return dp[mousePos][catPos][time]=2;
+		
+		if(dp[mousePos][catPos][time]!=-1) return dp[mousePos][catPos][time];
+		
+		int turn=time%2;
+		
+		
+		if(turn==0) {//mouse's turn
+			boolean flag=true;//cat wins
+			for(int i=0;i<mat[mousePos].length;i++) {
+				int ans=solver(mat, mat[mousePos][i], catPos, time+1, dp);
+				if(ans==1) return dp[mousePos][catPos][time]=1;
+				else if(ans!=2) flag=false;
+			}
+			if(flag) return dp[mousePos][catPos][time]=2;
+			else return dp[mousePos][catPos][time]=0;
+		}else {//cat's turn
+			boolean flag=true;//mouse wins
+			for(int i=0;i<mat[catPos].length;i++) {
+                if(mat[catPos][i]==0) continue;
+				int ans=solver(mat, mousePos, mat[catPos][i], time+1, dp);
+				if(ans==2) return dp[mousePos][catPos][time]=2;
+				else if(ans!=1) flag=false;
+			}
+			if(flag) return dp[mousePos][catPos][time]=1;
+			else return dp[mousePos][catPos][time]=0;
+		}
+		
+	}
 }
