c15a6d455a7efada06f17e31fad6de49bfedd2cb
Shreyansh Singh <65785547+ShreyanshSingh20@users.noreply.github.com>
2022-05-16 20:10:20 +0530

    Time: 26 ms (51.34%), Space: 58.5 MB (60.34%) - LeetHub

---
+++ 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.java
@@ -0,0 +1,38 @@
+class Solution {
+    public int shortestPathBinaryMatrix(int[][] mat) {
+        if(mat[0][0]==1) return -1;
+        int n=mat.length;
+        Queue<int[]> queue=new ArrayDeque<>();
+        queue.add(new int[]{0,0,1});
+        boolean vis[][]=new boolean[n][n];
+        vis[0][0]=true;
+        while(!queue.isEmpty()){
+            int size=queue.size();
+            for(int i=0;i<size;i++){
+                int curr[]=queue.poll();
+                int x=curr[0];int y=curr[1];int dist=curr[2];
+                if(x==n-1&&y==n-1) return dist;
+                for(int j=0;j<offset.length;j++){
+                    int newx=x+offset[j][0];
+                    int newy=y+offset[j][1];
+                    if(newx<0||newy<0||newx>=n||newy>=n||mat[newx][newy]==1||vis[newx][newy]) continue;
+                    queue.add(new int[]{newx,newy,dist+1});
+                    vis[newx][newy]=true;
+                }
+            }
+        }
+        
+        return -1;
+    }
+    
+    public int offset[][]={
+        {-1,-1},
+        {-1,0},
+        {-1,1},
+        {0,-1},
+        {0,1},
+        {1,-1},
+        {1,0},
+        {1,1}
+    };
+}
