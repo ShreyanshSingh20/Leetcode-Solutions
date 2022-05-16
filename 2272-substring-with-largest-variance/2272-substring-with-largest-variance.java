edfbb176d3fc57b913c30f36bc0ed214f05acf1d
Shreyansh Singh <65785547+ShreyanshSingh20@users.noreply.github.com>
2022-05-16 23:30:22 +0530

    Time: 206 ms (100.00%), Space: 43 MB (33.33%) - LeetHub

---
+++ 2272-substring-with-largest-variance/2272-substring-with-largest-variance.java
@@ -0,0 +1,34 @@
+class Solution {
+    public int largestVariance(String s) {
+        char c[]=s.toCharArray();
+        
+        int res=0;
+        
+        for(char highFreq='a';highFreq<='z';highFreq++){
+            for(char lowFreq='a';lowFreq<='z';lowFreq++){
+                if(highFreq==lowFreq) continue;
+                
+                int hf=0;int lf=0;int ans=0;boolean abandoned=false;
+                for(char ch:c){
+                    if(ch==highFreq) hf++;
+                    else if(ch==lowFreq) lf++;
+                    
+                    if(lf>0){
+                        ans=Math.max(ans,hf-lf);
+                    }else{
+                        if(abandoned){
+                            ans=Math.max(ans,hf-1);
+                        }
+                    }
+                    
+                    if(lf>hf){
+                        lf=0;hf=0;abandoned=true;
+                    }
+                }
+                res=Math.max(res,ans);
+            }
+        }
+        
+        return res;
+    }
+}
