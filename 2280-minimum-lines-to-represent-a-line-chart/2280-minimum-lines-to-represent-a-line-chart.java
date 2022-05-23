637521d5fc791e188574f7fc9cdf9a907e49685c
Shreyansh Singh <65785547+ShreyanshSingh20@users.noreply.github.com>
2022-05-23 16:58:22 +0530

    Time: 49 ms (66.67%), Space: 104.5 MB (44.44%) - LeetHub

---
+++ 2280-minimum-lines-to-represent-a-line-chart/2280-minimum-lines-to-represent-a-line-chart.java
@@ -0,0 +1,20 @@
+import java.math.BigDecimal;
+class Solution {
+    public int minimumLines(int[][] arr) {
+        int n=arr.length;
+        if(n==1) return 0;
+        Arrays.sort(arr,(a,b)->(a[0]-b[0]));
+        List<BigDecimal> list=new ArrayList<>();
+        
+        int res=n-1;
+        
+       for (int i = 1; i < n-1; i++) {
+           int x1=arr[i-1][0];int y1=arr[i-1][1];
+           int x2=arr[i][0];int y2=arr[i][1];
+           int x3=arr[i+1][0];int y3=arr[i+1][1];
+           if(1L*(y2-y1)*(x3-x2)==1L*(y3-y2)*(x2-x1)) res--;
+		}
+        
+        return res;
+    }
+}
