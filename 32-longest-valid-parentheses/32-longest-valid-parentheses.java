c8804b8c4f28b3393c8f85eba18cf620a6f8d0d9
Shreyansh Singh <65785547+ShreyanshSingh20@users.noreply.github.com>
2022-05-24 17:43:01 +0530

    Time: 6 ms (51.26%), Space: 42.4 MB (76.69%) - LeetHub

--- 32-longest-valid-parentheses/32-longest-valid-parentheses.java
+++ 32-longest-valid-parentheses/32-longest-valid-parentheses.java
@@ -1,29 +1,23 @@
 class Solution {
-    public int longestValidParentheses(String s) {
-        char c[]=s.toCharArray();
-        int n=c.length;
-        if(n==0) return 0;
-        int dp[]=new int[n+1];
-        dp[0]=0;dp[1]=0;
-        for(int i=1;i<n;i++){
-            if(c[i]==')'){
-                if(c[i-1]=='('){
-                    if(i==1) dp[i+1]=2;
-                    else dp[i+1]=dp[i-1]+2;
-                }else if(c[i-1]==')'){
-                    if(i-dp[i]-1>=0&&c[i-dp[i]-1]=='(') dp[i+1]=dp[i-dp[i]-1]+dp[i]+2;
+    public int longestValidParentheses(String str) {
+        char c[]=str.toCharArray();
+        Stack<Integer> s=new Stack<>();
+        s.push(-1);
+        
+        int res=0;
+        
+        for(int i=0;i<c.length;i++){
+            if(c[i]=='(') s.push(i);
+            else{
+                s.pop();
+                if(s.isEmpty()){
+                    s.push(i);
                 }else{
-                    dp[i+1]=0;
+                    res=Math.max(res,i-s.peek());
                 }
-            }else{
-                dp[i+1]=0;
             }
         }
         
-        int max=Integer.MIN_VALUE;
-        for(int x:dp) max=Math.max(max,x);
-        
-        return max;
+        return res;
     }
-    
 }
