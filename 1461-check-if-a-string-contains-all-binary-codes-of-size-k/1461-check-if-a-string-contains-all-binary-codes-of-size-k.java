519fbfae7ef673c4071e90771bd944f42fcdbf78
Shreyansh Singh <65785547+ShreyanshSingh20@users.noreply.github.com>
2022-05-31 16:11:54 +0530

    Time: 17 ms (92.71%), Space: 62.3 MB (60.94%) - LeetHub

--- 1461-check-if-a-string-contains-all-binary-codes-of-size-k/1461-check-if-a-string-contains-all-binary-codes-of-size-k.java
+++ 1461-check-if-a-string-contains-all-binary-codes-of-size-k/1461-check-if-a-string-contains-all-binary-codes-of-size-k.java
@@ -1,22 +1,18 @@
 class Solution {
     public boolean hasAllCodes(String str, int k) {
         char c[]=str.toCharArray();
-		int x=(int)Math.pow(2, k)-1;
-		int total=(int)Math.pow(2, k);
-		StringBuilder s=new StringBuilder();
-		int l=0;int cnt=0;HashSet<Integer> set=new HashSet<>();
+		int need=(1<<k);
+        boolean got[]=new boolean[need];
+        int allOne=need-1;
+        int hashValue=0;
 		for(int r=0;r<c.length;r++) {
-			s.append(c[r]);
-			if(r-l+1==k) {
-                // System.out.println(s.toString());
-				int num=Integer.parseInt(s.toString(), 2);
-                s.deleteCharAt(0);
-				l++;
-				if(set.contains(num)) continue;
-				if((num|x)==x) cnt++;
-				set.add(num);
-			}
+			hashValue=((hashValue<<1)&(allOne))|(c[r]-'0');
+            if(r>=k-1&&!got[hashValue]){
+                got[hashValue]=true;
+                need--;
+                if(need==0) return true;
+            }
 		}
-		return (cnt==total);
+		return false;
     }
 }
