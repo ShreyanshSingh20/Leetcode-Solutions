fb6114e057dc30ee4938031fc5f7a555f5ed6e16
Shreyansh Singh <65785547+ShreyanshSingh20@users.noreply.github.com>
2022-06-17 22:20:38 +0530

    Time: 2 ms (76.02%), Space: 42.3 MB (35.69%) - LeetHub

--- 291-word-pattern-ii/291-word-pattern-ii.java
+++ 291-word-pattern-ii/291-word-pattern-ii.java
@@ -11,28 +11,37 @@ class Solution {
 		if(i==pattern.length()||j==s.length()) return false;
 		
 		char c=pattern.charAt(i);
+        //if the character is already mapped to a string
 		if(dp.containsKey(c)) {
 			String str=dp.get(c);
 			
+            //check if this character can again be mapped to the same string..if no return false
 			if(!s.startsWith(str,j)) return false;
 			
+            //if yes, move to the next character and to the next part of the string
 			return solver(pattern, i+1, s, j+str.length(), dp, set);
 		}
 		
+        //if the character has not yet been mapped then try different substrings with which it can be mapped
 		for(int k=j;k<s.length();k++) {
 			String subString=s.substring(j, k+1);
 			
+            //if this substring had already been mapped then skip this as this will result in collisions
 			if(set.contains(subString)) continue;
 			
+            //update the dp state 
 			dp.put(c, subString);
 			set.add(subString);
 			
+            //check if I can get an answer with this configuration, if yes return true
 			if(solver(pattern, i+1, s, k+1, dp, set)) return true;
 			
+            //if no, then undo the configuration and move to the next substring in the next iteration in this loop
 			dp.remove(c);
 			set.remove(subString);
 		}
 		
+        //We can't do anything even after all these trys so just give up and return false
 		return false;
 	}
 }
