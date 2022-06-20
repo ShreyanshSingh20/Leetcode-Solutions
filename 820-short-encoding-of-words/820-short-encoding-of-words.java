984c7d26b885ead5612c1fd0fb6a3b9438b5a4d2
Shreyansh Singh <65785547+ShreyanshSingh20@users.noreply.github.com>
2022-06-20 10:47:17 +0530

    Time: 35 ms (57.05%), Space: 48.9 MB (84.66%) - LeetHub

--- 820-short-encoding-of-words/820-short-encoding-of-words.java
+++ 820-short-encoding-of-words/820-short-encoding-of-words.java
@@ -1,62 +1,112 @@
 class Solution {
-    public int minimumLengthEncoding(String[] words) {
-        int n = words.length;
-        
-		List<Pair> list = new ArrayList<>();
-		for (int i = 0; i < n; i++) {
-			list.add(new Pair(words[i], words[i].length()));
-		}
+		public int minimumLengthEncoding(String[] words) {
+			int n = words.length;
+            List<Pair> list = new ArrayList<>();
+            for (int i = 0; i < n; i++) {
+                list.add(new Pair(words[i], words[i].length()));
+            }
 
-		Collections.sort(list, new Comparator<Pair>() {
-			@Override
-			public int compare(Pair o1, Pair o2) {
-				return o1.len - o2.len;
+            Collections.sort(list, new Comparator<Pair>() {
+                @Override
+                public int compare(Pair o1, Pair o2) {
+                    return o1.len - o2.len;
+                }
+            });
+            
+			Trie trie = new Trie();
+			for (int i = 0; i < n; i++) {
+				StringBuilder s = new StringBuilder(list.get(i).str);
+				String rev = s.reverse().toString();
+				trie.insert(rev);
 			}
-		});
 
-		int ans = bfs(list);
-		return ans;
-    }
-    
-    public int bfs(List<Pair> list) {
-		int n = list.size();
-		boolean vis[] = new boolean[n];
-		StringBuilder res = new StringBuilder();
-
-		for (int i = 0; i < n; i++) {
-			if (!vis[i]) {
-				res.append(bfsSolver(list, vis, i));
-			}
+			String called = trie.caller();
+            System.out.println(called);
+			return (called.length());
 		}
-		// System.out.println(res.toString());
-		return res.length();
 	}
 
-	public String bfsSolver(List<Pair> list, boolean vis[], int source) {
-		Queue<Integer> queue = new ArrayDeque<>();
-		queue.add(source);
-		vis[source] = true;
-		int lastInd = -1;
-		while (!queue.isEmpty()) {
-			int currInd = queue.poll();
-			lastInd = currInd;
-			String currString = list.get(currInd).str;
-			for (int j = 0; j < list.size(); j++) {
-				if (!vis[j]) {
-					String str = list.get(j).str;
-					if (str.endsWith(currString)) {
-						queue.add(j);
-						vis[j] = true;
-						break;
-					}
+	class Node {
+		Node arr[];
+		String finString;
+        boolean eow;
+
+		public Node() {
+			arr = new Node[26];
+			finString = null;
+            eow=false;
+		}
+
+		public Node getChild(int i) {
+			return arr[i];
+		}
+
+		public void setChild(int i, Node newNode) {
+			arr[i] = newNode;
+		}
+
+		public void setString(String str) {
+			this.finString = str;
+		}
+        
+        public void markTrue(){
+            this.eow=true;
+        }
+        
+        public void markFalse(){
+            this.eow=false;
+        }
+        
+        public boolean getEOW(){
+            return eow;
+        }
+	}
+
+	class Trie {
+		Node root;
+		StringBuilder res;
+		Node tempNode;
+
+		public Trie() {
+			root = new Node();
+			res = new StringBuilder();
+			this.tempNode = root;
+		}
+
+		public void insert(String str) {
+			Node temp = root;
+			for (char c : str.toCharArray()) {
+				if (temp.getChild(c - 'a') == null) {
+					Node newNode = new Node();
+					temp.setChild(c - 'a', newNode);
+				}
+				temp = temp.getChild(c - 'a');
+                if(temp.getEOW()==true) temp.markFalse();
+			}
+			temp.setString(str);
+            temp.markTrue();
+		}
+
+		public String caller() {
+			dfs(tempNode);
+			return res.toString();
+		}
+
+		public void dfs(Node node) {
+
+			for (int i = 0; i < 26; i++) {
+				if (node.getChild(i) != null) {
+					dfs(node.getChild(i));
 				}
 			}
+
+			if (node.finString!=null&&node.getEOW()) {
+				res.append(node.finString);
+				res.append("#");
+			}
+
 		}
-
-		return list.get(lastInd).str + "#";
 	}
-
-}
 
 class Pair {
 		String str;
