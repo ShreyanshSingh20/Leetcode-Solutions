e1ba285347543fcaa02f0d374c86a0a9c6a9e8d5
Shreyansh Singh <65785547+ShreyanshSingh20@users.noreply.github.com>
2022-06-27 15:15:33 +0530

    Time: 66 ms (52.52%), Space: 73 MB (36.27%) - LeetHub

--- 630-course-schedule-iii/630-course-schedule-iii.java
+++ 630-course-schedule-iii/630-course-schedule-iii.java
@@ -1,24 +1,31 @@
-public class Solution {
+class Solution {
     public int scheduleCourse(int[][] courses) {
-        System.out.println(courses.length);
-        Arrays.sort(courses, (a, b) -> a[1] - b[1]);
-        int time = 0, count = 0;
-        for (int i = 0; i < courses.length; i++) {
-            if (time + courses[i][0] <= courses[i][1]) {
-                time += courses[i][0];
-                count++;
-            } else {
-                int max_i = i;
-                for (int j = 0; j < i; j++) {
-                    if (courses[j][0] > courses[max_i][0])
-                        max_i = j;
-                }
-                if (courses[max_i][0] > courses[i][0]) {
-                    time += courses[i][0] - courses[max_i][0];
-                }
-                courses[max_i][0] = -1;
+        
+        int n=courses.length;
+        
+        //sort the array in ascending order of last days
+        Arrays.sort(courses,(a,b)->(a[1]-b[1]));
+        
+        PriorityQueue<int[]> pq=new PriorityQueue<>((a,b)->(b[0]-a[0]));
+        
+        int time=0;
+        for(int i=0;i<n;i++){
+            int duration=courses[i][0];
+            int lastDay=courses[i][1];
+            if(time+duration<=lastDay){
+                time+=duration;
+                pq.add(new int[]{duration,lastDay});
+            }else{
+                if(pq.isEmpty()) continue;
+                int top[]=pq.peek();
+                if(top[0]<duration) continue;
+                int diff=top[0]-duration;
+                time-=(diff);
+                pq.poll();
+                pq.add(new int[]{duration,lastDay});
             }
         }
-        return count;
+        
+        return pq.size();
     }
 }
