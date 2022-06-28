0382537c922429598738d72f22f911902e6f3413
Shreyansh Singh <65785547+ShreyanshSingh20@users.noreply.github.com>
2022-06-28 20:47:32 +0530

    Time: 2 ms (100.00%), Space: 42 MB (98.75%) - LeetHub

--- 99-recover-binary-search-tree/99-recover-binary-search-tree.java
+++ 99-recover-binary-search-tree/99-recover-binary-search-tree.java
@@ -7,16 +7,44 @@ class Solution {
     b.val = tmp;
   }
 
+  public TreeNode getRightMostNode(TreeNode left, TreeNode curr) {
+    while (left.right != null && left.right != curr) left = left.right;
+    return left;
+  }
+
   public void findTwoSwapped(TreeNode root) {
-      if(root==null) return;
-      findTwoSwapped(root.left);
-      if(pred!=null&&root.val<pred.val){
-          y=root;
-          if(x==null) x=pred;
-          else return;
+    TreeNode curr = root;
+    while (curr != null) {
+      TreeNode left = curr.left;
+
+      if (left == null) {
+        //logic
+        if (pred != null && curr.val < pred.val) {
+          y = curr;
+          if (x == null) x = pred;
+        }
+        pred = curr;
+        curr = curr.right;
+      } else {
+        TreeNode rightMostNode = getRightMostNode(left, curr);
+
+        if (rightMostNode.right == null) {
+          //we need to create the thread
+          rightMostNode.right = curr;
+          curr = curr.left;
+        } else {
+          //we need to destroy the pre-existing thread
+          rightMostNode.right = null;
+          //logic
+          if (pred != null && curr.val < pred.val) {
+            y = curr;
+            if (x == null) x = pred;
+          }
+          pred = curr;
+          curr = curr.right;
+        }
       }
-      pred=root;
-      findTwoSwapped(root.right);
+    }
   }
 
   public void recoverTree(TreeNode root) {
