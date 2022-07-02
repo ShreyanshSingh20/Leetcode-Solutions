a04e052d67504db9b98abff766c1a05be7ae90f3
Shreyansh Singh <65785547+ShreyanshSingh20@users.noreply.github.com>
2022-07-02 17:24:43 +0530

    Time: 31 ms (6.52%), Space: 59.5 MB (70.20%) - LeetHub

---
+++ 1465-maximum-area-of-a-piece-of-cake-after-horizontal-and-vertical-cuts/1465-maximum-area-of-a-piece-of-cake-after-horizontal-and-vertical-cuts.java
@@ -0,0 +1,42 @@
+class Solution {
+    
+    public long mod=1_000_000_007;
+    
+    public int maxArea(int h, int w, int[] horizontalCuts, int[] verticalCuts) {
+        
+        Arrays.sort(horizontalCuts);
+        Arrays.sort(verticalCuts);
+        
+        //getting the max horizontal length
+        long maxHoz=Long.MIN_VALUE;
+        int lenHoz=horizontalCuts.length;
+        for(int i=0;i<lenHoz;i++){
+            if(i==0){
+                maxHoz=Math.max(maxHoz,horizontalCuts[i]);
+                continue;
+            }
+            maxHoz=Math.max(maxHoz,horizontalCuts[i]-horizontalCuts[i-1]);
+        }
+        maxHoz=Math.max(maxHoz,h-horizontalCuts[lenHoz-1]);
+        
+        //getting the max vertical length
+        long maxVer=Long.MIN_VALUE;
+        int lenVer=verticalCuts.length;
+        for(int i=0;i<lenVer;i++){
+            if(i==0){
+                maxVer=Math.max(maxVer,verticalCuts[i]);
+                continue;
+            }
+            maxVer=Math.max(maxVer,verticalCuts[i]-verticalCuts[i-1]);
+        }
+        maxVer=Math.max(maxVer,w-verticalCuts[lenVer-1]);
+        
+        System.out.println(maxHoz+" "+maxVer);
+        
+        return (int)mul(maxHoz,maxVer);
+    }
+    
+    public long mul(long x,long y){
+        return (x*y)%mod;
+    }
+}
