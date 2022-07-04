d95ef1e901caae3687f07eed30d5b1b8383e3e46
Shreyansh Singh <65785547+ShreyanshSingh20@users.noreply.github.com>
2022-07-04 18:44:43 +0530

    Time: 0 ms (100.00%), Space: 40.1 MB (85.39%) - LeetHub

--- 376-wiggle-subsequence/376-wiggle-subsequence.java
+++ 376-wiggle-subsequence/376-wiggle-subsequence.java
@@ -1,33 +1,18 @@
 class Solution {
     public int wiggleMaxLength(int[] arr) {
         int n=arr.length;
-        int dpUp[]=new int[n];
-        int dpDown[]=new int[n];
-        Arrays.fill(dpUp,Integer.MIN_VALUE);
-        Arrays.fill(dpDown,Integer.MIN_VALUE);
-        dpUp[0]=dpDown[0]=1;
         
-        int res=1;
+        int lastUp=1;
+        int lastDown=1;
         
         for(int i=1;i<n;i++){
-          
-            //considering this element is a peak up element
-            for(int j=0;j<i;j++){
-                if(arr[i]>arr[j]&&dpDown[j]!=Integer.MIN_VALUE){
-                    dpUp[i]=Math.max(dpUp[i],1+dpDown[j]);
-                }
+            if(arr[i]>arr[i-1]){
+                lastUp=1+lastDown;
+            }else if(arr[i]<arr[i-1]){
+                lastDown=1+lastUp;
             }
-            
-            //considering this element is a peak down element
-            for(int j=0;j<i;j++){
-                if(arr[i]<arr[j]&&dpUp[j]!=Integer.MIN_VALUE){
-                    dpDown[i]=Math.max(dpDown[i],1+dpUp[j]);
-                }
-            }
-            
-            res=Math.max(res,Math.max(dpUp[i],dpDown[i]));
         }
         
-        return res;
+        return Math.max(lastUp,lastDown);
     }
 }
