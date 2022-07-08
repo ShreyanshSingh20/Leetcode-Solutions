31c0367f7fd4df0c6db80438335f974e1d97b98d
Shreyansh Singh <65785547+ShreyanshSingh20@users.noreply.github.com>
2022-07-08 12:06:11 +0530

    Time: 1 ms (99.65%), Space: 40.3 MB (98.06%) - LeetHub

--- 97-interleaving-string/97-interleaving-string.java
+++ 97-interleaving-string/97-interleaving-string.java
@@ -1,31 +1,36 @@
 class Solution {
     public boolean isInterleave(String s1, String s2, String s3) {
         
-        char a[]=s1.toCharArray();
-        char b[]=s2.toCharArray();
-        char c[]=s3.toCharArray();
+        if(s1.length()+s2.length()!=s3.length()) return false;
         
-        Boolean dp[][][]=new Boolean[a.length+1][b.length+1][c.length+1];
-        return solver(a,b,c,0,0,0,dp);
+        Boolean dp[][]=new Boolean[s1.length()+1][s2.length()+1];
         
+        return solver(s1,0,s2,0,s3,0,dp);
     }
     
-    public boolean solver(char a[],char b[],char c[],int i,int j,int k,Boolean dp[][][]){
+    public boolean solver(String s1,int i,String s2,int j,String s3,int k,Boolean dp[][]){
         
-        if(i==a.length&&j==b.length&&k==c.length) return true;
-        if(k>=c.length&&(i<a.length||j<b.length)) return false;
-        if(k<c.length&&(i==a.length&&j==b.length)) return false;
-        
-        if(dp[i][j][k]!=null) return dp[i][j][k];
-        
-        if(i<a.length&&j<b.length&&k<c.length&&a[i]==c[k]&&b[j]==c[k]){
-            return dp[i][j][k]=(solver(a,b,c,i+1,j,k+1,dp)||solver(a,b,c,i,j+1,k+1,dp));
-        }else if(i<a.length&&k<c.length&&a[i]==c[k]){
-            return dp[i][j][k]=solver(a,b,c,i+1,j,k+1,dp);
-        }else if(j<b.length&&k<c.length&&b[j]==c[k]){
-            return dp[i][j][k]=solver(a,b,c,i,j+1,k+1,dp);
+        if(i==s1.length()){
+            return s2.substring(j).equals(s3.substring(k));
         }
         
-        return dp[i][j][k]=false;
+        if(j==s2.length()){
+            return s1.substring(i).equals(s3.substring(k));
+        }
+        
+        if(dp[i][j]!=null) return dp[i][j];
+        
+        if(i<s1.length()&&j<s2.length()&&k<s3.length()&&
+           s1.charAt(i)==s3.charAt(k)&&s2.charAt(j)==s3.charAt(k)){
+            return dp[i][j]=(solver(s1,i+1,s2,j,s3,k+1,dp)||solver(s1,i,s2,j+1,s3,k+1,dp));
+        }else if(i<s1.length()&&k<s3.length()&&
+                    s1.charAt(i)==s3.charAt(k)){
+            return dp[i][j]=solver(s1,i+1,s2,j,s3,k+1,dp);
+        }else if(j<s2.length()&&k<s3.length()&&
+                    s2.charAt(j)==s3.charAt(k)){
+            return dp[i][j]=solver(s1,i,s2,j+1,s3,k+1,dp);
+        }
+        
+        return dp[i][j]=false;
     }
 }
