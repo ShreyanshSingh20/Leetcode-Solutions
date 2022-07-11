275fc36cb21363d712645d78235f66a80a9f79a4
Shreyansh Singh <65785547+ShreyanshSingh20@users.noreply.github.com>
2022-07-11 11:04:27 +0530

    Time: 28 ms (50.00%), Space: 107.9 MB (12.50%) - LeetHub

---
+++ 2333-minimum-sum-of-squared-difference/2333-minimum-sum-of-squared-difference.java
@@ -0,0 +1,26 @@
+class Solution {
+    public long minSumSquareDiff(int[] a, int[] b, int k1, int k2) {
+        
+        int n=a.length;
+        int ops=k1+k2;
+        
+        int bucket[]=new int[100005];
+        for(int i=0;i<n;i++){
+            bucket[Math.abs(a[i]-b[i])]++;
+        }
+        
+        for(int i=bucket.length-1;i>0;i--){
+            int freq=Math.min(ops,bucket[i]);
+            ops-=freq;
+            bucket[i-1]+=freq;
+            bucket[i]-=freq;
+        }
+        
+        long res=0;
+        for(int i=0;i<bucket.length;i++){
+            res=res+(long)(bucket[i])*(long)i*(long)i;
+        }
+        
+        return res;
+    }
+}
