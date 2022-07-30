11bad6a284d5e014c001ee9bb97919dc5601f987
Shreyansh Singh <65785547+ShreyanshSingh20@users.noreply.github.com>
2022-07-30 20:46:34 +0530

    Time: 3 ms (20.37%), Space: 42 MB (36.16%) - LeetHub

---
+++ 1024-video-stitching/1024-video-stitching.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int videoStitching(int[][] clips, int time) {
+        
+        //let dp[t]->represent the minimum number of clips taken to cover till time 't'
+        int dp[]=new int[105];
+        Arrays.fill(dp,Integer.MAX_VALUE/4);
+        dp[0]=0;
+        
+        for(int t=0;t<=time;t++){
+            for(int clip[]:clips){
+                int start=clip[0];int end=clip[1];
+                //if the starting time is smaller than current time 't' and the 
+                //ending time is bigger than the current time 't' then we can effectively take this
+                //clip to contribute to our answer
+                if(start<=time&&end>=t){
+                    dp[t]=Math.min(dp[t],dp[start]+1);
+                }
+            }
+            if(dp[t]>=time+1) return -1;
+        }
+        
+        return dp[time];
+    }
+}
