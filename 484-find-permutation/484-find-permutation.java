d4029426ca6104873ec96ccff102b072cefcb73d
Shreyansh Singh <65785547+ShreyanshSingh20@users.noreply.github.com>
2022-08-01 22:17:06 +0530

    Time: 3 ms (90.43%), Space: 51.5 MB (69.15%) - LeetHub

--- 484-find-permutation/484-find-permutation.java
+++ 484-find-permutation/484-find-permutation.java
@@ -1,34 +1,30 @@
 class Solution {
     public int[] findPermutation(String s) {
-        char c[]=s.toCharArray();
-		int n=c.length+1;
-		int arr[]=new int[n];
-		for(int i=0;i<n;i++)
-			arr[i]=i+1;
-		
-		int l=0;int r=0;
-		while(r<c.length) {
-			if(c[r]=='I') {
-				reverse(arr,l,r);
-				l=r+1;
-				r++;
-				continue;
-			}
-			r++;
-		}
-		
-		reverse(arr,l,r);
         
-        return arr;
+        char c[]=s.toCharArray();
+        int res[]=new int[c.length+1];
+        for(int i=0;i<c.length+1;i++){
+            res[i]=i+1;
+        }
+        
+        int l=0;int r=0;
+        for(r=0;r<c.length;r++){
+            if(c[r]=='I'){
+                reverse(res,l,r);
+                l=r+1;
+            }
+        }
+        reverse(res,l,r);
+        return res;
     }
     
-    public void reverse(int arr[],int l,int r) {
-		int templ=l;int tempr=r;
-		while(templ<=tempr) {
-			int temp=arr[templ];
-			arr[templ]=arr[tempr];
-			arr[tempr]=temp;
-			templ++;tempr--;
-		}
-	}
+    public void reverse(int arr[],int l,int r){
+        int templ=l;int tempr=r;
+        while(templ<=tempr){
+            int temp=arr[templ];
+            arr[templ]=arr[tempr];
+            arr[tempr]=temp;
+            templ++;tempr--;
+        }
+    }
 }
