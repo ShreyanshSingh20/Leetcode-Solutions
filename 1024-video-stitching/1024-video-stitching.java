bf9da0efae408724b30423434e9b03025ce20659
Shreyansh Singh <65785547+ShreyanshSingh20@users.noreply.github.com>
2022-08-11 00:22:28 +0530

    Time: 2 ms (55.27%), Space: 41.6 MB (64.10%) - LeetHub

--- 1024-video-stitching/1024-video-stitching.java
+++ 1024-video-stitching/1024-video-stitching.java
@@ -1,23 +1,16 @@
 class Solution {
-    public int videoStitching(int[][] clips, int time) {
-        
-        //let dp[t]->represent the minimum number of clips taken to cover till time 't'
-        int dp[]=new int[105];
-        Arrays.fill(dp,Integer.MAX_VALUE/4);
-        dp[0]=0;
-        
-        for(int t=0;t<=time;t++){
-            for(int clip[]:clips){
-                int start=clip[0];int end=clip[1];
-                //if the starting time is smaller than current time 't' and the 
-                //ending time is bigger than the current time 't' then we can effectively take this
-                //clip to contribute to our answer
-                if(start<=time&&end>=t){
-                    dp[t]=Math.min(dp[t],dp[start]+1);
-                }
+    public int videoStitching(int[][] clips, int T) {
+        int end = 0, farCanReach = 0, cnt = 0;
+        Arrays.sort(clips, (a, b) -> (a[0] - b[0])); // sort by start time
+        for(int i = 0; end < T; end = farCanReach) {   // at the beginning of each step, we need update the "end"           
+            cnt++;                       
+            while(i < clips.length && clips[i][0] <= end) { // check all elements within the range
+                farCanReach = Math.max(farCanReach, clips[i++][1]); // update the "farCanReach" for next step
             }
-        }
-        
-        return dp[time]==Integer.MAX_VALUE/4?-1:dp[time];
+            if(end == farCanReach) return -1;   
+			// if "farCanReach" isn't updated after we checked all elements within the range, we will fail in next step. 
+			// say the first element in next step is [curS, curE], "curS" must be larger than "end" = "farCanReach".
+        }		
+        return cnt;
     }
 }
