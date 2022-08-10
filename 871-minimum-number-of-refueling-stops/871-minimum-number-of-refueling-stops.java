3b189355148370796ac108f815f80faa0cb81c88
Shreyansh Singh <65785547+ShreyanshSingh20@users.noreply.github.com>
2022-08-10 20:54:27 +0530

    Time: 6 ms (54.74%), Space: 49.3 MB (26.16%) - LeetHub

--- 871-minimum-number-of-refueling-stops/871-minimum-number-of-refueling-stops.java
+++ 871-minimum-number-of-refueling-stops/871-minimum-number-of-refueling-stops.java
@@ -7,17 +7,27 @@ class Solution {
         for(int x[]:stations){
             int location=x[0];
             int capacity=x[1];
+            //subtract the fuel from tank which we have used up from moving from i-1 to i;
             tank-=(location-prev);
+            
+            //if the tank becomes negative then we should have refilled the tank before
             while(!pq.isEmpty()&&tank<0){
                 tank+=pq.poll();
                 cnt++;
             }
             
+            //if the tank is still empty then we can't do anything
             if(tank<0) return -1;
+            
+            //add the current offering at the current gas station
             pq.add(capacity);
+            
+            //set the prev to the current location
             prev=location;
         }
         
+        
+        //check again for target
         tank-=(target-prev);
         while(!pq.isEmpty()&&tank<0){
             tank+=pq.poll();
