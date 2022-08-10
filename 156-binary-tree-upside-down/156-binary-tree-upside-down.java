76e493d5382e7671c817ddc9fb8d2ca9bcc3abf1
Shreyansh Singh <65785547+ShreyanshSingh20@users.noreply.github.com>
2022-08-10 19:19:33 +0530

    Time: 0 ms (100.00%), Space: 40.3 MB (89.04%) - LeetHub

--- 156-binary-tree-upside-down/156-binary-tree-upside-down.java
+++ 156-binary-tree-upside-down/156-binary-tree-upside-down.java
@@ -15,29 +15,16 @@
  */
 class Solution {
     public TreeNode upsideDownBinaryTree(TreeNode root) {
-        if(root==null) return null;
-        List<List<TreeNode>> list=new ArrayList<>();
-        Queue<TreeNode> q=new ArrayDeque<>();
-        int pt=-1;
-        q.add(root);
         
-        while(!q.isEmpty()){
-            int size=q.size();
-            if(size>0) {pt++; list.add(new ArrayList<TreeNode>());}
-            for(int i=0;i<size;i++){
-                TreeNode curr=q.poll();
-                TreeNode newNode=new TreeNode(curr.val);
-                list.get(pt).add(newNode);
-                if(curr.left!=null) q.add(curr.left);
-                if(curr.right!=null) q.add(curr.right);
-            }
-        }
+        if(root==null||(root.left==null&&root.right==null)) return root;
         
-        for(int i=1;i<list.size();i++){
-            if(list.get(i).size()>1) list.get(i).get(0).left=list.get(i).get(1);
-            list.get(i).get(0).right=list.get(i-1).get(0);
-        }
+        TreeNode newRoot=upsideDownBinaryTree(root.left);
         
-        return list.get(list.size()-1).get(0);
+        root.left.left=root.right;
+        root.left.right=root;
+        
+        root.left=null;
+        root.right=null;
+        return newRoot;
     }
 }
