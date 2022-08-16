235c78d59ea1c33c574318a10b1d54587236e43a
Shreyansh Singh <65785547+ShreyanshSingh20@users.noreply.github.com>
2022-08-17 01:59:35 +0530

    Time: 0 ms (100.00%), Space: 43 MB (44.11%) - LeetHub

--- 88-merge-sorted-array/88-merge-sorted-array.java
+++ 88-merge-sorted-array/88-merge-sorted-array.java
@@ -1,56 +1,28 @@
 class Solution {
-    public void merge(int[] a, int m, int[] b, int n) {
-        int pt=m;
-        int l=0;int r=0;
-        if(n==0) return;
-        while(l<m&&r<n){
-            if(a[l]<=b[r]){
-                a[pt]=a[l];
-                l++;
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        
+        int l=m-1;int r=n-1;
+        int pt=m+n-1;
+        while(l>=0&&r>=0){
+            if(nums1[l]>=nums2[r]){
+                nums1[pt]=nums1[l];
+                l--;
             }else{
-                a[pt]=b[r];
-                r++;
+                nums1[pt]=nums2[r];
+                r--;
             }
-            pt=(pt+1)%a.length;
+            pt=pt-1;
         }
         
-        while(l<m){
-            a[pt]=a[l];
-            l++;
-            pt=(pt+1)%a.length;
-        }
-        while(r<n){
-            a[pt]=b[r];
-            r++;
-            pt=(pt+1)%a.length;
+        while(l>=0){
+            nums1[pt]=nums1[l];
+            l--;pt--;
         }
         
-        // for(int x:a) System.out.print(x+" ");
-        
-        int ind=-1;
-        for(int i=1;i<a.length;i++){
-            if(a[i]<a[i-1]){
-                ind=i;
-                break;
-            }
+        while(r>=0){
+            nums1[pt]=nums2[r];
+            r--;pt--;
         }
-        if(ind==-1) return;
-        int k=a.length-ind;
         
-        int len=a.length;
-        k=k%len;
-        reverse(a,0,len-1);//reverse whole array
-        reverse(a,0,k-1);//reverse first k elements
-        reverse(a,k,len-1);//reverse the last n-k elements
-        
-    }
-    
-     public void reverse(int arr[],int l,int r){
-        while(l<=r){
-            int temp=arr[l];
-            arr[l]=arr[r];
-            arr[r]=temp;
-            l++;r--;
-        }
     }
 }
