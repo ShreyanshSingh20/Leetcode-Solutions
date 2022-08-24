c80967edb2b0d694ea08390c6ed874311f6c2762
Shreyansh Singh <65785547+ShreyanshSingh20@users.noreply.github.com>
2022-08-24 19:19:29 +0530

    Time: 199 ms (88.16%), Space: 78.4 MB (85.28%) - LeetHub

--- 2102-sequentially-ordinal-rank-tracker/2102-sequentially-ordinal-rank-tracker.java
+++ 2102-sequentially-ordinal-rank-tracker/2102-sequentially-ordinal-rank-tracker.java
@@ -1,36 +1,50 @@
 class SORTracker {
-    TreeSet<Location> locations = new TreeSet<>(); 
-    Location lastReturned = new Location("", Integer.MAX_VALUE); // Last returned (or equivalent)
+    
+    TreeSet<Location> set;
+    Location lastUsed=new Location("",Integer.MAX_VALUE);
+
+    public SORTracker() {
+        this.set=new TreeSet<>();
+    }
     
     public void add(String name, int score) {
-        Location location = new Location(name, score);
-        locations.add(location);
+        Location newLocation=new Location(name,score);
+        set.add(newLocation);
         
-		// If the new location is before the last returned, we need to update the pointer
-        if (location.compareTo(lastReturned) < 0) {
-            lastReturned = locations.lower(lastReturned);
+        if(newLocation.compareTo(lastUsed)<0){
+            lastUsed=set.lower(lastUsed);
         }
     }
     
     public String get() {
-        // Update the pointer
-        lastReturned = locations.higher(lastReturned);
         
-        return lastReturned.name;
+        lastUsed=set.higher(lastUsed);
+        return lastUsed.name;
     }
     
- class Location implements Comparable<Location> {
+    class Location implements Comparable<Location>{
         String name;
         int score;
-        
-        public Location(String name, int score) {
-            this.name = name;
-            this.score = score;
+
+        public Location(String name,int score){
+            this.name=name;
+            this.score=score;
         }
-        
-        public int compareTo(Location l) {
-            // Sort by score (desc), then name (asc)
-            return score != l.score ? -Integer.compare(score, l.score) : name.compareTo(l.name);
+
+        public int compareTo(Location l1){
+            return score==l1.score?name.compareTo(l1.name):(-Integer.compare(score,l1.score));
         }
     }
+    
 }
+
+
+
+
+
+/**
+ * Your SORTracker object will be instantiated and called as such:
+ * SORTracker obj = new SORTracker();
+ * obj.add(name,score);
+ * String param_2 = obj.get();
+ */
