990152b129fca6fe5bb35f5a7c62ae43d1cd22f1
Shreyansh Singh <65785547+ShreyanshSingh20@users.noreply.github.com>
2022-08-26 00:05:03 +0530

    Time: 119 ms (94.53%), Space: 42.9 MB (96.16%) - LeetHub

--- 1074-number-of-submatrices-that-sum-to-target/1074-number-of-submatrices-that-sum-to-target.java
+++ 1074-number-of-submatrices-that-sum-to-target/1074-number-of-submatrices-that-sum-to-target.java
@@ -1,34 +1,40 @@
 class Solution {
-    public int numSubmatrixSumTarget(int[][] mat, int target) {
-        int r=mat.length;
-        int c=mat[0].length;
+    public int numSubmatrixSumTarget(int[][] matrix, int target) {
+        
+        int r=matrix.length;
+        int c=matrix[0].length;
+        
         int psum[][]=new int[r+1][c+1];
-        psum[1][1]=mat[0][0];
-       
-        for(int i=1;i<=r;i++){
-            for(int j=1;j<=c;j++){
-                psum[i][j]=mat[i-1][j-1]+(psum[i-1][j]+psum[i][j-1]-psum[i-1][j-1]);
+        for(int i=1;i<r+1;i++){
+            for(int j=1;j<c+1;j++){
+                psum[i][j]=(matrix[i-1][j-1]+psum[i-1][j]+psum[i][j-1])-psum[i-1][j-1];
             }
         }
+        
+        // for(int i=0;i<=r;i++){
+        //     for(int j=0;j<=c;j++){
+        //         System.out.print(psum[i][j]+" ");
+        //     }
+        //     System.out.println();
+        // }
         
         HashMap<Integer,Integer> map=new HashMap<>();
-        int cnt=0;
-        for(int r1=1;r1<=r;r1++){
+        int res=0;
+        for(int r1=1;r1<r+1;r1++){
             for(int r2=1;r2<=r1;r2++){
                 map.clear();
-                for(int col=1;col<=c;col++){
-                    int subMatSum=psum[r1][col]-psum[r2-1][col];
-                    if(subMatSum==target) cnt++;
-                    int key=subMatSum-target;
-                    if(map.containsKey(key)){
-                        cnt+=map.get(key);
+                for(int j=1;j<c+1;j++){
+                    int sum=psum[r1][j]-psum[r2-1][j];
+                    int sub=sum-target;
+                    if(map.containsKey(sub)){
+                        res+=map.get(sub);
                     }
-                    map.putIfAbsent(subMatSum,0);
-                    map.put(subMatSum,map.get(subMatSum)+1);
+                    map.put(sum,map.getOrDefault(sum,0)+1);
                 }
+                if(map.containsKey(target)) res+=map.get(target);
             }
         }
         
-        return cnt;
+        return res;
     }
 }
