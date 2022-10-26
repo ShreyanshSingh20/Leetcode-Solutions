4e73ece14a23de4e56969438368d23561b50cfe8
Shreyansh Singh <65785547+ShreyanshSingh20@users.noreply.github.com>
2022-10-26 21:58:12 +0530

    Time: 219 ms (24.39%), Space: 74.9 MB (11.79%) - LeetHub

--- 0523-continuous-subarray-sum/0523-continuous-subarray-sum.js
+++ 0523-continuous-subarray-sum/0523-continuous-subarray-sum.js
@@ -3,32 +3,31 @@
  * @param {number} k
  * @return {boolean}
  */
-var checkSubarraySum = function(nums, k) {
-    
-    const n = nums.length;
-    
-    const map = new Map();
-    
-    let prefSum = [nums[0]];
-    
-    for(let i = 1 ; i < n ; i++){
-        prefSum.push(prefSum[i-1] + nums[i]); 
+var checkSubarraySum = function (nums, k) {
+  const n = nums.length;
+
+  const map = new Map();
+
+  let prefSum = [nums[0]];
+
+  for (let i = 1; i < n; i++) {
+    prefSum.push(prefSum[i - 1] + nums[i]);
+  }
+
+  let sum = 0;
+
+  for (let i = 0; i < n; i++) {
+    sum += nums[i];
+    const mod = sum % k;
+    if (mod === 0 && i + 1 >= 2) return true;
+    if (map.has(mod)) {
+      const prevInd = map.get(mod);
+      if (i - prevInd >= 2 && (sum - prefSum[prevInd]) % k == 0) return true;
     }
-    
-    let sum = 0;
-    
-    for(let i = 0 ; i < n ; i++){
-        sum += nums[i];
-        const mod = sum % k;
-        if(mod === 0 && i+1 >=2) return true;
-        if(map.has(mod)){
-            const prevInd = map.get(mod);
-            if(i-prevInd >= 2 && (sum-prefSum[prevInd])%k == 0) return true;
-        }
-        if(!map.has(mod)){
-            map.set(mod,i);
-        }
+    if (!map.has(mod)) {
+      map.set(mod, i);
     }
-    
-    return false;
+  }
+
+  return false;
 };
